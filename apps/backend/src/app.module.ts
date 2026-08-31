7c0d475d83c9f0cd6364e98b341c24a1fa0da06a
mageshyt <mageshytdev@gmail.com>
2026-08-31 22:38:27 +0530

    [API]:Started Working on Auth API

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Modules } from './modules/.module';
 
 @Module({
-  imports: [],
+  imports: [Modules],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
