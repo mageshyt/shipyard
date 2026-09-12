49905f5b0e0694bc705e2010747672410fcd1e62
mageshyt <mageshytdev@gmail.com>
2026-09-12 23:36:49 +0530

    [Types]: Add volume contract types

--- packages/types/src/docker/index.ts
+++ packages/types/src/docker/index.ts
@@ -2,3 +2,4 @@ export * from './container';
 export * from './health';
 export * from './image';
 export * from './network';
+export * from './volume';
---
+++ packages/types/src/docker/volume.ts
@@ -0,0 +1,24 @@
+export interface VolumeUsageData {
+  Size: number;
+  RefCount: number;
+}
+
+export interface DockerVolume {
+  Name: string;
+  Driver: string;
+  Mountpoint: string;
+  CreatedAt?: string;
+  Labels: Record<string, string>;
+  Scope: string;
+  Options: Record<string, string> | null;
+  UsageData?: VolumeUsageData | null;
+}
+
+export interface CreateVolume {
+  name: string;
+}
+
+export interface DeleteVolume {
+  name: string;
+  force?: boolean;
+}
