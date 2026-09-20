ce041558b76cdbd1281eb1bd4eca92ae2e871c2f
mageshyt <mageshytdev@gmail.com>
2026-09-20 21:30:36 +0530

    [Types]: Add deployment job contracts
    - CreateDeployment (source/branch/commitHash) and DeploymentJobData (ids-only queue payload)

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,5 +1,7 @@
 export * from './auth';
 export * from './common';
+export * from './deployment';
 export * from './docker';
 export * from './project';
 export * from './service';
+export * from './jobs';
